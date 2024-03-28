58b3071ba5dae5ce55a22a4ed6310b190240b6e9
Arend Neels <arn@pmgroup.be>
2024-03-28 11:24:22 +0100

    Added more SymposiumViewModels

--- src/models/symposium.ts
+++ src/models/symposium.ts
@@ -8,3 +8,22 @@ export interface SymposiumPostResultViewModel {
     name: string;
     imageUrl: string;
 }
+
+export interface SymposiumPostViewModel {
+    id: number;
+    name: string; 
+    description: string;
+    imageUrl: string;
+    addressId: number;
+}
+
+export interface SymposiumContactInfoViewModel {
+    firstName: string;
+    lastName: string;
+    email: string; 
+    mobile: string | null;
+    phone: string | null;
+    website: string | null;
+    linkedin: string | null;
+    facebook: string | null;
+}
