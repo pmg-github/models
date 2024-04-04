5edba0084c40489391ddb2f7d193a4339950703f
pmg-sda <sda@pmg.be>
2024-04-04 14:25:30 +0200

    nrOfOpened run build

--- dist/models/newsletter.d.ts
+++ dist/models/newsletter.d.ts
@@ -2,4 +2,5 @@ export declare class NewsletterProjectModel {
     code: string | undefined;
     name: string | undefined;
     releaseDate: Date | undefined;
+    numberOfOpened: number | undefined;
 }
--- dist/models/newsletter.js
+++ dist/models/newsletter.js
@@ -2,4 +2,5 @@ export class NewsletterProjectModel {
     code = undefined;
     name = undefined;
     releaseDate = undefined;
+    numberOfOpened = undefined;
 }
