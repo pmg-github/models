06db51ecff16f22254de36d17b228bb341f72d8a
pmg-sda <sda@pmg.be>
2024-04-04 14:22:34 +0200

    numberOfOpened

--- src/models/newsletter.ts
+++ src/models/newsletter.ts
@@ -2,4 +2,5 @@ export class NewsletterProjectModel {
   code: string | undefined = undefined;
   name: string | undefined = undefined;
   releaseDate: Date | undefined = undefined;
+  numberOfOpened: number | undefined = undefined;
 }
