9b1e95ec8f39aa13b1411f72e8bc47c0792c9286
Arend Neels <arn@pmgroup.be>
2024-05-28 09:25:28 +0200

    Updated ProefAboCreateModel

--- dist/models/abos.d.ts
+++ dist/models/abos.d.ts
@@ -1,19 +1,6 @@
 export declare class ProefAboCreateModel {
     portalCode: string;
     lang: string;
-    email: string;
-    firstName: string;
-    lastName: string;
-    phone: string;
-    password: string;
-    street: string;
-    streetNumber: string;
-    streetBoxNumber: string | undefined;
-    zipCode: string;
-    city: string;
-    countryCode: string;
-    type: string;
-    vat: string;
-    companyName: string;
+    addressId: number;
     constructor(data: Partial<ProefAboCreateModel>);
 }
--- dist/models/abos.js
+++ dist/models/abos.js
@@ -14,21 +14,7 @@ const class_validator_1 = require("class-validator");
 class ProefAboCreateModel {
     portalCode;
     lang;
-    email;
-    firstName;
-    lastName;
-    phone;
-    password;
-    // adress
-    street;
-    streetNumber;
-    streetBoxNumber = undefined;
-    zipCode;
-    city;
-    countryCode;
-    type;
-    vat;
-    companyName;
+    addressId;
     constructor(data) {
         Object.assign(this, data);
     }
@@ -46,54 +32,6 @@ __decorate([
 ], ProefAboCreateModel.prototype, "lang", void 0);
 __decorate([
     (0, class_validator_1.IsNotEmpty)(),
-    (0, class_validator_1.IsEmail)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "email", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "firstName", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "lastName", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "phone", void 0);
-__decorate([
-    (0, class_validator_1.MinLength)(6),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "password", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "street", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "streetNumber", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "zipCode", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "city", void 0);
-__decorate([
-    (0, class_validator_1.IsNotEmpty)(),
-    (0, class_validator_1.Length)(2, 2),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "countryCode", void 0);
-__decorate([
-    (0, class_validator_1.ValidateIf)((obj, val) => obj.type == "b2b"),
-    (0, class_validator_1.IsNotEmpty)(),
-    (0, class_validator_1.Matches)(/^(ATU|BE0|BG|CHE|CY|CZ|DE|DK|EE|EL|ES|FI|FR|GB|GR|HR|HU|IE|IT|LT|LU|LV|MT|NL|PL|PT|RO|SE|SI|SK)[\s-.]?[0-9A-Z]{2,12}[\s-.]?/),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "vat", void 0);
-__decorate([
-    (0, class_validator_1.ValidateIf)((obj, val) => obj.type == "b2b"),
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", String)
-], ProefAboCreateModel.prototype, "companyName", void 0);
+    (0, class_validator_1.IsNumber)(),
+    __metadata("design:type", Number)
+], ProefAboCreateModel.prototype, "addressId", void 0);
