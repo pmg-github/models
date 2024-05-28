09a5e59a2d0f81364715b8be9f4a3792d04c8943
Arend Neels <arn@pmgroup.be>
2024-05-28 09:11:24 +0200

    Updated ProefAboCreateModel

--- src/models/abos.ts
+++ src/models/abos.ts
@@ -1,11 +1,4 @@
-import {
-  IsEmail,
-  IsNotEmpty,
-  MinLength,
-  ValidateIf,
-  Matches,
-  Length,
-} from "class-validator";
+import { IsNotEmpty, Length, IsNumber } from "class-validator";
 
 export class ProefAboCreateModel {
   @IsNotEmpty()
@@ -17,52 +10,8 @@ export class ProefAboCreateModel {
   lang!: string;
 
   @IsNotEmpty()
-  @IsEmail()
-  email!: string;
-
-  @IsNotEmpty()
-  firstName!: string;
-
-  @IsNotEmpty()
-  lastName!: string;
-
-  @IsNotEmpty()
-  phone!: string;
-
-  @MinLength(6)
-  password!: string;
-
-  // adress
-  @IsNotEmpty()
-  street!: string;
-
-  @IsNotEmpty()
-  streetNumber!: string;
-
-  streetBoxNumber: string | undefined = undefined;
-
-  @IsNotEmpty()
-  zipCode!: string;
-
-  @IsNotEmpty()
-  city!: string;
-
-  @IsNotEmpty()
-  @Length(2, 2)
-  countryCode!: string;
-
-  type!: string;
-
-  @ValidateIf((obj: ProefAboCreateModel, val: string) => obj.type == "b2b")
-  @IsNotEmpty()
-  @Matches(
-    /^(ATU|BE0|BG|CHE|CY|CZ|DE|DK|EE|EL|ES|FI|FR|GB|GR|HR|HU|IE|IT|LT|LU|LV|MT|NL|PL|PT|RO|SE|SI|SK)[\s-.]?[0-9A-Z]{2,12}[\s-.]?/
-  )
-  vat!: string;
-
-  @ValidateIf((obj: ProefAboCreateModel, val: string) => obj.type == "b2b")
-  @IsNotEmpty()
-  companyName!: string;
+  @IsNumber()
+  addressId!: number;
 
   constructor(data: Partial<ProefAboCreateModel>) {
     Object.assign(this, data);
