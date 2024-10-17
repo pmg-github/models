5860c6352b0d60783f357df4637981c61ea1ac3c
Arend Neels <arn@pmgroup.be>
2024-10-17 08:58:51 +0200

    Added interests property to ContextViewModel

--- dist/models/context.d.ts
+++ dist/models/context.d.ts
@@ -1,5 +1,7 @@
+import { InterestViewModel } from "./interest";
 export interface ContextViewModel {
     id: number;
     code: string;
     name: string;
+    interests: InterestViewModel[] | undefined;
 }
--- src/models/context.ts
+++ src/models/context.ts
@@ -1,5 +1,8 @@
+import { InterestViewModel } from "./interest";
+
 export interface ContextViewModel {
   id: number;
   code: string;
   name: string;
+  interests: InterestViewModel[] | undefined;
 }
