c94230f0a4f30fb3d7b20919c31074abbf79503f
pmg-sda <sda@pmg.be>
2024-11-14 10:56:53 +0100

    run build :-(

--- dist/models/pmguser.d.ts
+++ dist/models/pmguser.d.ts
@@ -1,6 +1,8 @@
 export declare class PmgUserModel {
-    id: number | undefined;
-    code: string | undefined;
-    firstName: string | undefined;
-    lastName: string | undefined;
+    id?: number | undefined;
+    code?: string | undefined;
+    firstName?: string | undefined;
+    lastName?: string | undefined;
+    constructor(id?: number | undefined, code?: string | undefined, firstName?: string | undefined, lastName?: string | undefined);
 }
+export type PmgUserInterface = Partial<PmgUserModel>;
--- dist/models/pmguser.js
+++ dist/models/pmguser.js
@@ -2,9 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PmgUserModel = void 0;
 class PmgUserModel {
-    id = undefined;
-    code = undefined;
-    firstName = undefined;
-    lastName = undefined;
+    id;
+    code;
+    firstName;
+    lastName;
+    constructor(id, code, firstName, lastName) {
+        this.id = id;
+        this.code = code;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
 exports.PmgUserModel = PmgUserModel;
