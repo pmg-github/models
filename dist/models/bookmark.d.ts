99d17f6c31aea4f936a7eac9a22bb216d959d281
pmg-sda <sda@pmg.be>
2024-12-04 11:31:57 +0100

    index.ts > bookmarks

---
+++ dist/models/bookmark.d.ts
@@ -0,0 +1,14 @@
+export interface BookmarkListModel {
+    id: number;
+    userId: number;
+    folderName: string;
+    title: string;
+    description: string;
+    url: string;
+    target: string;
+}
+export interface BookmarkFolderListModel {
+    id: number;
+    userId: number;
+    name: string;
+}
