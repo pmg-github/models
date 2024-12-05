34c1f5c273adbdc74b2116ca0d42dda4c7393469
pmg-sda <sda@pmg.be>
2024-12-05 13:40:33 +0100

    EmployeeWorkSchedule added

--- dist/models/employee.d.ts
+++ dist/models/employee.d.ts
@@ -3,8 +3,21 @@ export interface EmployeesSearchListModel {
     firstName: string;
     lastName: string;
     internalPhoneNumber: string;
+    privateMobileNumber: string;
     teamName: string;
     functionName: string;
     presenceStatus: string;
     lastActivityTime: string;
+    workSchedule: EmployeeWorkSchedule;
+}
+export interface EmployeeWorkSchedule {
+    dayOfWeek: number;
+    morning: {
+        startHour: string | null;
+        endHour: string | null;
+    };
+    afternoon: {
+        startHour: string | null;
+        endHour: string | null;
+    };
 }
--- src/models/employee.ts
+++ src/models/employee.ts
@@ -3,8 +3,22 @@ export interface EmployeesSearchListModel {
   firstName: string;
   lastName: string;
   internalPhoneNumber: string;
+  privateMobileNumber: string;
   teamName: string;
   functionName: string;
   presenceStatus: string;
   lastActivityTime: string;
+  workSchedule: EmployeeWorkSchedule;
+}
+
+export interface EmployeeWorkSchedule {
+  dayOfWeek: number; // Huidige dag van de week (1 = maandag, 2 = dinsdag, etc.)
+  morning: {
+    startHour: string | null; // Startuur voormiddag
+    endHour: string | null; // Einduur voormiddag
+  };
+  afternoon: {
+    startHour: string | null; // Startuur namiddag
+    endHour: string | null; // Einduur namiddag
+  };
 }
