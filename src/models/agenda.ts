9189879120bdb438ed99b95dac7a60f72e1a2d4d
Doran Delfosse <ddf@pmgroup.be>
2024-12-06 10:21:30 +0100

    feat: wright type for title in eventListModel

--- src/models/agenda.ts
+++ src/models/agenda.ts
@@ -2,7 +2,7 @@ import { IsEnum, IsNotEmpty, IsString } from "class-validator";
 
 export interface EventListModel {
   id: number;
-  title: number;
+  title: string;
   description: string;
   startDateTime: Date;
   endDateTime: Date;
