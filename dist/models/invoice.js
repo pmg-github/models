5c3e052f9a8666a687f3cb4002e36260f2b82767
pmg-sda <sda@pmg.be>
2024-12-13 09:49:21 +0100

    added filepath to invoice pdf

--- dist/models/invoice.js
+++ dist/models/invoice.js
@@ -1,2 +1,52 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.UnpaidInvoicesListModel = void 0;
+const INVOICE_FOLDER = "\\\\bkhserver\\BoekhDocumenten\\Facturen\\";
+class UnpaidInvoicesListModel {
+    pmgCompanyName;
+    pmCompanyFolder;
+    customerNumber;
+    customerCompanyName;
+    customerEmail;
+    customerTelephone;
+    customerStreet;
+    customerStreetNumber;
+    customerStreetBoxNumber;
+    customerZipCode;
+    customerCity;
+    customerCountryCode;
+    contactFirstname;
+    contactLastname;
+    contactEmail;
+    contactMobile;
+    contactTelephone;
+    rappelLanguageCode;
+    rappelPostpone;
+    rappelPostponeUntil;
+    invoiceNumber;
+    invoiceDate;
+    invoiceExpirationDate;
+    invoiceDaysOverdue;
+    invoiceAmount;
+    invoiceUnpaidAmount;
+    get invoiceYear() {
+        return this.invoiceDate
+            ? this.invoiceDate.getFullYear().toString()
+            : undefined;
+    }
+    get invoiceFolder() {
+        return this.invoiceNumber
+            ? this.invoiceNumber.substring(0, 3) + "000"
+            : undefined;
+    }
+    get invoiceFilePath() {
+        if (!this.invoiceYear ||
+            !this.invoiceFolder ||
+            !this.invoiceNumber ||
+            !this.pmgCompanyName) {
+            return undefined;
+        }
+        return `${INVOICE_FOLDER}${this.pmgCompanyName}\\${this.invoiceYear}\\${this.invoiceFolder}\\${this.invoiceNumber}.PDF`;
+    }
+}
+exports.UnpaidInvoicesListModel = UnpaidInvoicesListModel;
--- src/models/invoice.ts
+++ src/models/invoice.ts
@@ -1,29 +1,56 @@
-export interface UnpaidInvoicesListModel {
-  pmgCompanyName: string;
-  customerNumber: string;
-  customerCompanyName: string;
-  customerEmail: string;
-  customerTelephone: string;
-  customerStreet: string;
-  customerStreetNumber: string;
-  customerStreetBoxNumber: string;
-  customerZipCode: string;
-  customerCity: string;
-  customerCountryCode: string;
-  contactFirstname: string;
-  contactLastname: string;
-  contactEmail: string;
-  contactMobile: string;
-  contactTelephone: string;
-  rappelLanguageCode: string;
-  rappelPostpone: number;
-  rappelPostponeUntil: Date;
-  invoiceNumber: string;
-  invoiceDate: Date;
-  invoiceExpirationDate: Date;
-  invoiceDaysOverdue: number;
-  invoiceAmount: number;
-  invoiceUnpaidAmount: number;
+const INVOICE_FOLDER = "\\\\bkhserver\\BoekhDocumenten\\Facturen\\";
+export class UnpaidInvoicesListModel {
+  pmgCompanyName?: string;
+  pmCompanyFolder?: string;
+  customerNumber?: string;
+  customerCompanyName?: string;
+  customerEmail?: string;
+  customerTelephone?: string;
+  customerStreet?: string;
+  customerStreetNumber?: string;
+  customerStreetBoxNumber?: string;
+  customerZipCode?: string;
+  customerCity?: string;
+  customerCountryCode?: string;
+  contactFirstname?: string;
+  contactLastname?: string;
+  contactEmail?: string;
+  contactMobile?: string;
+  contactTelephone?: string;
+  rappelLanguageCode?: string;
+  rappelPostpone?: number;
+  rappelPostponeUntil?: Date;
+  invoiceNumber?: string;
+  invoiceDate?: Date;
+  invoiceExpirationDate?: Date;
+  invoiceDaysOverdue?: number;
+  invoiceAmount?: number;
+  invoiceUnpaidAmount?: number;
+
+  get invoiceYear(): string | undefined {
+    return this.invoiceDate
+      ? this.invoiceDate.getFullYear().toString()
+      : undefined;
+  }
+
+  get invoiceFolder(): string | undefined {
+    return this.invoiceNumber
+      ? this.invoiceNumber.substring(0, 3) + "000"
+      : undefined;
+  }
+
+  get invoiceFilePath(): string | undefined {
+    if (
+      !this.invoiceYear ||
+      !this.invoiceFolder ||
+      !this.invoiceNumber ||
+      !this.pmgCompanyName
+    ) {
+      return undefined;
+    }
+
+    return `${INVOICE_FOLDER}${this.pmgCompanyName}\\${this.invoiceYear}\\${this.invoiceFolder}\\${this.invoiceNumber}.PDF`;
+  }
 }
 
 export interface PmgCompanyList {
