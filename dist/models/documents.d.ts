3e8d40faff42404736b51ba0f575356f9fe19963
Doran Delfosse <ddf@pmgroup.be>
2025-03-20 08:59:21 +0100

    update DocumentTypeViewModel to include url

--- dist/models/documents.d.ts
+++ dist/models/documents.d.ts
@@ -1,4 +1,5 @@
 export interface DocumentTypeViewModel {
     id: number;
     name: string;
+    url?: string;
 }
--- src/models/documents.ts
+++ src/models/documents.ts
@@ -1,5 +1,5 @@
 export interface DocumentTypeViewModel {
-    id:number
-    name:string
-  }
-  
+  id: number;
+  name: string;
+  url?: string;
+}
