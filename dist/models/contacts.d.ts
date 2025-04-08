7176a4e96443c62f8466bd5127b885045cf3fa61
Arend Neels <arn@pmgroup.be>
2025-04-08 11:23:23 +0200

    Added interface for PMGTeamMemberViewModel

---
+++ dist/models/contacts.d.ts
@@ -0,0 +1,10 @@
+export interface PMGTeamMemberViewModel {
+    parentCode: string;
+    firstName: string;
+    lastName: string;
+    name: string;
+    position: string;
+    email: string;
+    imageUrl: string;
+    department: string;
+}
