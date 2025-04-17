66944f572fab5d9964d2d3964d102ac42c665cad
Doran Delfosse <ddf@pmgroup.be>
2025-04-17 15:32:44 +0200

    forget to build

--- dist/models/bestCoice.d.ts
+++ dist/models/bestCoice.d.ts
@@ -1,6 +1,7 @@
 import { BcCompanyViewModel } from "./company";
 export interface BestChoiceCategories {
     code: string;
+    name: string;
     categories: {
         code: string;
         name: string;
