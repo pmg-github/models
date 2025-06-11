4afb45edc740c2b9bc64c752a589a6e1d95a38a7
Doran Delfosse <ddf@pmg.be>
2025-06-11 13:12:08 +0200

    test CreatePopupDto

--- dist/models/popup.js
+++ dist/models/popup.js
@@ -87,7 +87,9 @@ __decorate([
 class CreatePopupDto {
     id;
     portals;
+    // @Type(() => PopupMlDto)
     nl;
+    // @Type(() => PopupMlDto)
     fr;
     isActive;
     dateFrom;
@@ -110,13 +112,15 @@ __decorate([
     __metadata("design:type", Array)
 ], CreatePopupDto.prototype, "portals", void 0);
 __decorate([
-    (0, class_validator_1.ValidateNested)(),
-    (0, class_transformer_1.Type)(() => PopupMlDto),
+    (0, class_validator_1.ValidateNested)()
+    // @Type(() => PopupMlDto)
+    ,
     __metadata("design:type", PopupMlDto)
 ], CreatePopupDto.prototype, "nl", void 0);
 __decorate([
-    (0, class_validator_1.ValidateNested)(),
-    (0, class_transformer_1.Type)(() => PopupMlDto),
+    (0, class_validator_1.ValidateNested)()
+    // @Type(() => PopupMlDto)
+    ,
     __metadata("design:type", PopupMlDto)
 ], CreatePopupDto.prototype, "fr", void 0);
 __decorate([
