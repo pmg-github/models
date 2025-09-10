6af76dcf4c72639020e5df8fd0327921b16eb0ee
Doran Delfosse <ddf@pmg.be>
2025-09-10 16:07:20 +0200

    update SelectOptionViewModel

--- src/models/selectoption.ts
+++ src/models/selectoption.ts
@@ -27,7 +27,7 @@ class IsStringOrNumberConstraint implements ValidatorConstraintInterface {
 export interface SelectOptionViewModel {
   // New property names to use instead of id/name => remove old properties when other code is replaced!
   key: string;
-  value: number | string;
+  value: number | string | boolean;
 }
 
 export class SelectOptionViewModelDto {
