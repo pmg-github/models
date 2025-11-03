c6c2f83df46bb2626c32ec515b935c32ca206c8e
Doran Delfosse <ddf@pmg.be>
2025-11-03 13:08:39 +0100

    update BoArticleMetaDataModel

--- src/models/article.ts
+++ src/models/article.ts
@@ -279,12 +279,13 @@ export interface BoArticleMetaDataModel {
   displayAuthor: string;
   availableFrom: string | Date | null;
   availableUntil: string | Date | null;
-  creatorParentCode: string;
-  updaterParentCode: string;
-  ownerParentCode: string;
-  translatorParentCode: string;
+  creatorParentCode: PmgUserInterface;
+  updaterParentCode: PmgUserInterface;
+  ownerParentCode: PmgUserInterface;
+  translatorParentCode: PmgUserInterface;
   newsletterMoreLabel: string;
   productId: number | null;
+  updatedAt: Date;
 }
 
 export interface BoArticleFileModel {
