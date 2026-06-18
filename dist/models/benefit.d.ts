50f248367b3067ecb7df65f3c6524f7c8a5224b2
Dave Stappers <sda@pmgroup.be>
2026-06-18 08:58:13 +0200

    Did my model changes to dist file. Rookie mistake.

--- dist/models/benefit.d.ts
+++ dist/models/benefit.d.ts
@@ -1,31 +1,15 @@
 export interface BenefitViewModel {
     id: number;
-    internalName: string;
-    internalDescription: string | null;
     articleReference: string;
     typeId: number;
-    titleNL: string;
-    titleFR: string;
-    titleEN: string | null;
-    specsNL: string | null;
-    specsFR: string | null;
-    specsEN: string | null;
+    title: string;
+    specs: string | null;
     discountCode: string | null;
-    discountValueNL: string | null;
-    discountValueFR: string | null;
-    discountValueEN: string | null;
-    discountLabelNL: string | null;
-    discountLabelFR: string | null;
-    discountLabelEN: string | null;
-    buttonTextNL: string | null;
-    buttonTextFR: string | null;
-    buttonTextEN: string | null;
-    buttonUrlNL: string | null;
-    buttonUrlFR: string | null;
-    buttonUrlEN: string | null;
+    discountValue: string | null;
+    discountLabel: string | null;
+    buttonText: string | null;
+    buttonUrl: string | null;
     statusId: number;
     validFrom: string | Date | null;
     validUntil: string | Date | null;
-    createdAt: string | Date;
-    updatedAt: string | Date;
 }
