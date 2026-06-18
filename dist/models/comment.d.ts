e564c313fd64325680dd5dc809dca383fb0221ac
Doran Delfosse <ddf@pmg.be>
2026-06-18 16:51:02 +0200

    Enhance CommentListView interface by adding uid, data, isResolved, and comments properties

--- dist/models/comment.d.ts
+++ dist/models/comment.d.ts
@@ -4,6 +4,18 @@ export interface CommentListView {
     comment: string;
     dateTime: Date | string;
     user: PmgUserInterface;
+    uid: number | string;
+    data: unknown;
+    isResolved: number | boolean;
+    comments: {
+        uid: number | string;
+        type: CommentTypeView;
+        comment: string;
+        dateTime: Date | string;
+        user: PmgUserInterface;
+        data: unknown;
+        isResolved: number | boolean;
+    }[];
 }
 export interface CommentTypeView {
     id: number;
--- src/models/comment.ts
+++ src/models/comment.ts
@@ -1,10 +1,22 @@
 import { PmgUserInterface } from "./pmguser";
 
-export interface CommentListView{
-    type:CommentTypeView;
-    comment:string
-    dateTime:Date|string
-    user:PmgUserInterface
+export interface CommentListView {
+  type: CommentTypeView;
+  comment: string;
+  dateTime: Date | string;
+  user: PmgUserInterface;
+  uid: number | string;
+  data: unknown;
+  isResolved: number | boolean;
+  comments: {
+    uid: number | string;
+    type: CommentTypeView;
+    comment: string;
+    dateTime: Date | string;
+    user: PmgUserInterface;
+    data: unknown;
+    isResolved: number | boolean;
+  }[];
 }
 
 export interface CommentTypeView{
